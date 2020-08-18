303c3ea47b9bb6641f4ea963c2cc072ea2e8ecab
Marcos Ramos <rramos@pop-os.localdomain>
2020-08-17 22:08:05 -0300

    Added Roles enum and Authorities enum

--- src/main/java/com/amigoscode/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/amigoscode/demo/security/ApplicationSecurityConfig.java
@@ -1,23 +1,58 @@
 package com.amigoscode.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfig(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-                .antMatchers("/", "index", "/css/*", "/js/*")
-                .permitAll()
+                .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
+                .antMatchers("/api/**").hasRole(ApplicationUserRole.STUDENT.name())
                 .anyRequest()
                 .authenticated()
                 .and()
                 .httpBasic();
+    }
+
+    @Bean
+    @Override
+    protected UserDetailsService userDetailsService() {
+        UserDetails annaSmithUser = User.builder()
+                .username("annasmith")
+                .password(passwordEncoder.encode("pass"))
+                .roles(ApplicationUserRole.STUDENT.name())
+                .build();
+
+        UserDetails linda = User.builder()
+                .username("linda")
+                .password(passwordEncoder.encode("pass123"))
+                .roles(ApplicationUserRole.ADMIN.name())
+                .build();
+
+            return new InMemoryUserDetailsManager(
+                annaSmithUser,
+                linda
+        );
     }
 }
